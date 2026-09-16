d097c352cbfc064392e5e6b44ae9ff3651f2ce92
Maxime MONICHON--SIRONNEAU <maxime.monichonsironneau@gmail.com>
2026-09-16 15:56:23 +0200

    Exercice 4

--- src/net/lecnam/ecue2/tp3/Exec2.java
+++ src/net/lecnam/ecue2/tp3/Exec2.java
@@ -9,6 +9,11 @@ public class Exec2 {
         Point p = new Point(5, 6);
         Rectangle r2 = new Rectangle(p, 7, 8);
 
+        System.out.println(r1.retourneSurface());
+        System.out.println(r2.retourneSurface());
+
+        r2.translate(3, 2);
+
     }
 
 }
--- src/net/lecnam/ecue2/tp3/Rectangle.java
+++ src/net/lecnam/ecue2/tp3/Rectangle.java
@@ -2,20 +2,29 @@ package net.lecnam.ecue2.tp3;
 
 public class Rectangle {
 
-    double longueur, largeur, x, y; //Exercice 2
+    double longueur, largeur, longitude, latitude; //Exercice 2
 
     public Rectangle(Point p, double longueur, double largeur){
         this.longueur = longueur;
         this.largeur = largeur;
-        this.x = p.longitude;
-        this.y = p.latitude;
+        this.longitude = p.longitude;
+        this.latitude = p.latitude;
     }
 
     public Rectangle(double x, double y, double longueur, double largeur){
         this.longueur = longueur;
         this.largeur = largeur;
-        this.x = x;
-        this.y = y;
+        this.longitude = x;
+        this.latitude = y;
+    } // Exercice 3
+
+    public double retourneSurface(){
+        return this.longueur * this.largeur;
     }
+
+    public void translate(double x, double y){
+        this.longitude += x;
+        this.latitude += y;
+    } //Exercice 4
 
 }
