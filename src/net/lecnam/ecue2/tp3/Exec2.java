572433448f2bef1eaadb4b35bbabba25e1542814
Maxime MONICHON--SIRONNEAU <maxime.monichonsironneau@gmail.com>
2026-09-16 15:52:37 +0200

    Exercice 3

---
+++ src/net/lecnam/ecue2/tp3/Exec2.java
@@ -0,0 +1,14 @@
+package net.lecnam.ecue2.tp3;
+
+public class Exec2 {
+
+    public static void main(String[] args) {
+
+        Rectangle r1 = new Rectangle(5, 6, 7, 8);
+
+        Point p = new Point(5, 6);
+        Rectangle r2 = new Rectangle(p, 7, 8);
+
+    }
+
+}
--- src/net/lecnam/ecue2/tp3/Rectangle.java
+++ src/net/lecnam/ecue2/tp3/Rectangle.java
@@ -4,6 +4,18 @@ public class Rectangle {
 
     double longueur, largeur, x, y; //Exercice 2
 
-    
+    public Rectangle(Point p, double longueur, double largeur){
+        this.longueur = longueur;
+        this.largeur = largeur;
+        this.x = p.longitude;
+        this.y = p.latitude;
+    }
+
+    public Rectangle(double x, double y, double longueur, double largeur){
+        this.longueur = longueur;
+        this.largeur = largeur;
+        this.x = x;
+        this.y = y;
+    }
 
 }
