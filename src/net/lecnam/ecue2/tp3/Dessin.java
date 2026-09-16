9cfd0add06d4c16b0042c83d5341638a057f1f01
Maxime MONICHON--SIRONNEAU <maxime.monichonsironneau@gmail.com>
2026-09-16 16:24:54 +0200

    Exercice 7

---
+++ src/net/lecnam/ecue2/tp3/Dessin.java
@@ -0,0 +1,13 @@
+package net.lecnam.ecue2.tp3;
+
+public class Dessin {
+
+    Rectangle[] tableau;
+    int nbRec;
+
+    public Dessin(){
+        this.tableau = new Rectangle[10];
+        this.nbRec = 0;
+    } //Exercice 7
+
+}
---
+++ src/net/lecnam/ecue2/tp3/Exec3.java
@@ -0,0 +1,11 @@
+package net.lecnam.ecue2.tp3;
+
+public class Exec3 {
+
+    public static void main(String[] args) {
+
+        Dessin d = new Dessin();
+
+    }
+
+}
